c652c8e28c45a4323e522f2ae6e3e47ce8733627
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-07-07 17:35:24 +0800

    INIT

--- src/main/java/com/service/base/BaseApplication.java
+++ src/main/java/com/service/base/BaseApplication.java
@@ -2,6 +2,8 @@ package com.service.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -14,8 +16,13 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EnableEurekaClient
 @MapperScan(basePackages = {"com.service.base.mapper"})
 @ComponentScan(basePackages = {"com.scottxuan","com.service.base"})
-public class BaseApplication {
+public class BaseApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BaseApplication.class);
     }
 }
